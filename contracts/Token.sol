eb1d03f6c1f59e22b275623ced82975d359cc14e
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-03-17 16:01:33 +1000

    Feat:  Begin token

---
+++ contracts/Token.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+abstract contract MN8 is Ownable, ERC20 {
+
+    constructor() ERC20("MN8", "emanate") {
+        
+    }
+}
