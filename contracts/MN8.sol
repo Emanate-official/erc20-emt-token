4df981733c3876a92d13415529eb82740af07eb1
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-03-19 08:49:04 +1000

    Chore:  Add total supply

--- contracts/MN8.sol
+++ contracts/MN8.sol
@@ -7,6 +7,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract MN8 is Ownable, ERC20 {
 
     constructor() ERC20("emanate", "MN8") {
-        
+        _mint(msg.sender, 10000000000000000);
     }
 }
