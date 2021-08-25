f81a9640a1033251bbabb72bd06b0f32e3215401
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-08-25 12:18:19 +1000

    Chore: Clean up

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,8 +1,7 @@
+// https://docs.openzeppelin.com/upgrades-plugins/1.x/truffle-upgrades
 const Token = artifacts.require("Token");
 const { deployProxy } = require("@openzeppelin/truffle-upgrades");
 
 module.exports = async (deployer) => {
   await deployProxy(Token, ["Emanate", "EMT"], { deployer });
-
-  //await deployProxy(Token, ["Emanate", "EMT"]);
 };
