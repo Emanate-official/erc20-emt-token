c396190b8b2a9fa5ea7191e92a14d7cd739cd466
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-09-07 13:06:15 +1000

    Chore: Update deploy scripts

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,7 +1,10 @@
 // https://docs.openzeppelin.com/upgrades-plugins/1.x/truffle-upgrades
 const Token = artifacts.require("Token");
-const { deployProxy } = require("@openzeppelin/truffle-upgrades");
+const { deployProxy, upgradeProxy } = require("@openzeppelin/truffle-upgrades");
 
 module.exports = async (deployer) => {
-  await deployProxy(Token, ["Emanate", "EMT"], { deployer });
+  await deployProxy(Token, ["Emanate", "EMT"], { deployer, kind: "uups" });
+  // const existing = await Token.deployed();
+  // const instance = await upgradeProxy(existing.address, Token, { deployer, kind: "uups" });
+  // console.log("Upgraded", instance.address);
 };
